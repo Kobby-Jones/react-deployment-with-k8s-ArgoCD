97f966766904a468d618c4e857a3b7d9f8192609
Kobby Jones <kobbyj099@gmail.com>
2023-03-15 20:33:22 +0000

    For Rent and For Sale Buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import {Navbar, Nav} from 'react-bootstrap'
+import {Navbar, Nav, Button} from 'react-bootstrap'
  
 function CustomNavbar() {
     
     return (
-        <Navbar bg="dark" className='px-5' variant = "dark" expand = "md">
-            <Navbar.Brand href="#">KJ Real Estate Agency Ltd.</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id ="basic-navbar-nav">
-                <Nav className='ms-auto' >
-                    <Nav.Link href='#'>Home</Nav.Link>
-                    <Nav.Link href='#'>About</Nav.Link>
-                    <Nav.Link href='#'>Contact</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-       </Navbar>
-    )
+      <Navbar bg="dark" className="px-5 py-4" variant="dark" expand="md">
+        <Navbar.Brand href="#">KJ Real Estate Agency Ltd.</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+            <Button className="ms-5" variant="warning">
+              For Rent
+            </Button>
+            <Button className="ms-5" variant="warning">
+              For Sale
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
 }
 export default CustomNavbar;
