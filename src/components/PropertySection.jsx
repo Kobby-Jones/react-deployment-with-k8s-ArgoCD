bbf4af01a784e9fcafba9167a58623fb5790db98
Kobby Jones <kobbyj099@gmail.com>
2023-03-17 23:39:53 +0000

    Properties Section

---
+++ src/components/PropertySection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import PropertiesCard from './PropertiesCard';
+
+function PropertySection() {
+    return ( 
+        <Container>
+            <PropertiesCard />
+        </Container>
+     );
+}
+
+export default PropertySection;
