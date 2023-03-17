1a384908e46720396a66d8a4683af8ac3fc03984
Kobby Jones <kobbyj099@gmail.com>
2023-03-17 04:06:17 +0000

    customers object

--- src/components/Testiminials.jsx
+++ src/components/Testiminials.jsx
@@ -5,7 +5,7 @@ import TestimonialRow from './TestimonialRow';
 const Testimonials = () => {
     return (
         <div className='p-4'>
-            <h2 className='text-center'>Customer Testimonials</h2>
+            <h2 className='text-center mb-3'>Customer Testimonials</h2>
             <TestimonialRow />
         </div>
     )
