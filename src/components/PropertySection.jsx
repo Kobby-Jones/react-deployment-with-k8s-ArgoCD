f3616f30ae2870c33f625258c5ae5f22179a4cf3
Kobby Jones <kobbyj099@gmail.com>
2023-03-18 00:25:48 +0000

    Condo House

--- src/components/PropertySection.jsx
+++ src/components/PropertySection.jsx
@@ -10,6 +10,9 @@ function PropertySection() {
                 <Col lg="4" >
                      <PropertiesCard type={properties.forSale.singleFamHouse.type} imageUrl = {properties.forSale.singleFamHouse.image} address = {properties.forSale.singleFamHouse.address}  />
                 </Col>
+                <Col lg="4" >
+                     <PropertiesCard type={properties.forSale.condo.type} imageUrl = {properties.forSale.condo.image} address = {properties.forSale.condo.address}  />
+                </Col>
             </Row>
         </Container>
      );
