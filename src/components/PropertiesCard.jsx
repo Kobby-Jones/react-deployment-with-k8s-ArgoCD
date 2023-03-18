8588b13184baaae3e4272585db3c09144882715f
Kobby Jones <kobbyj099@gmail.com>
2023-03-18 00:41:44 +0000

    Updated properties Card

--- src/components/PropertiesCard.jsx
+++ src/components/PropertiesCard.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 function PropertiesCard(props) {
-    return ( 
-        <Card className='text-center p-3'>
-            <Card.Img variant="top" alt='House Image' src={props.imageUrl} />
-            <Card.Body>
-                <Card.Title>{props.type}</Card.Title>
-                <Card.Text>{ props.address}</Card.Text>
-                <Card.Text>{ props.bedRooms}</Card.Text>
-                <Card.Text>{props.bathRooms}</Card.Text>
-                <Button variant='primary'>View Property</Button>
-            </Card.Body>
-        </Card>
-     );
+    return (
+      <Card className="text-center p-3">
+        <Card.Img variant="top" alt="House Image" src={props.imageUrl} />
+        <Card.Body>
+          <Card.Title>{props.type}</Card.Title>
+          <Card.Text>{props.address}</Card.Text>
+          <Card.Text>
+            {props.bedRooms} Bedrooms | {props.bathRooms} Bathrooms{" "}
+          </Card.Text>
+          <Button variant="primary">View Property</Button>
+        </Card.Body>
+      </Card>
+    );
 }
 
 export default PropertiesCard;
--- src/components/PropertySection.jsx
+++ src/components/PropertySection.jsx
@@ -5,10 +5,10 @@ import properties from './Properties';
 
 function PropertySection() {
     return (
-        <section className="bg-info py-5">
-            <div className="text-center">
-                <h2 className='mb-3'>Properties For Sale</h2>
-            </div>
+      <section className="bg-info py-5">
+        <div className="text-center">
+          <h2 className="mb-3">Properties For Sale</h2>
+        </div>
         <Container>
           <Row>
             <Col lg="4">
@@ -16,6 +16,8 @@ function PropertySection() {
                 type={properties.forSale.singleFamHouse.type}
                 imageUrl={properties.forSale.singleFamHouse.image}
                 address={properties.forSale.singleFamHouse.address}
+                bedRooms={properties.forSale.singleFamHouse.bedRooms}
+                bathRooms={properties.forSale.singleFamHouse.bathRooms}
               />
             </Col>
             <Col lg="4">
@@ -23,6 +25,8 @@ function PropertySection() {
                 type={properties.forSale.condo.type}
                 imageUrl={properties.forSale.condo.image}
                 address={properties.forSale.condo.address}
+                bedRooms={properties.forSale.condo.bedRooms}
+                bathRooms={properties.forSale.condo.bathRooms}
               />
             </Col>
             <Col lg="4">
@@ -30,6 +34,8 @@ function PropertySection() {
                 type={properties.forSale.townHouse.type}
                 imageUrl={properties.forSale.townHouse.image}
                 address={properties.forSale.townHouse.address}
+                bedRooms={properties.forSale.townHouse.bedRooms}
+                bathRooms={properties.forSale.townHouse.bathRooms}
               />
             </Col>
           </Row>
