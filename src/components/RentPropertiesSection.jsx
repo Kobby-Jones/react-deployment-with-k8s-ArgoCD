25ef8823d244ed99e2d7a43fe84fcae0029d61fc
Kobby Jones <kobbyj099@gmail.com>
2023-03-19 01:39:03 +0000

    Render Rent Properties Card

--- src/components/RentPropertiesSection.jsx
+++ src/components/RentPropertiesSection.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import backgroundImage from '../img/background_img_1.jpg'
+import React from "react";
+import forRent from "./RentProperties";
+import { Container, Row, Col } from "react-bootstrap";
+import backgroundImage from "../img/background_img_1.jpg";
+import PropertiesCard from "./PropertiesCard";
 
 function RentPropertySection() {
-    return (
-      <section
-        className="bg-info py-5"
+  return (
+    <section
+      className="bg-info py-5"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+      }}
+    >
+      <div
+        className="text-center py-5"
         style={{
-          backgroundImage: `url(${backgroundImage})`,
-            backgroundAttachment: "fixed",
-          backgroundSize:'cover'
+          backgroundColor: "rgba(0,0,0,0.8)",
         }}
       >
-        <div
-          className="text-center py-5"
-          style={{
-            backgroundColor: "rgba(0,0,0,0.8)",
-          }}
-        >
-          <h2 className="mb-3 text-light fw-bolder">Properties For Rent</h2>
-        </div>
-      </section>
-    );
+        <h2 className="mb-3 text-light fw-bolder">Properties For Rent</h2>
+      </div>
+      <Container>
+        <Row>
+          {forRent.map((property, index) =>
+            Object.keys(property).map((propType) => (
+              <Col lg="4" className="mt-3" key={index}>
+                <PropertiesCard
+                  type={property[propType].type}
+                  imageUrl={property[propType].image}
+                  address={property[propType].address}
+                  bedRooms={property[propType].bedRooms}
+                  bathRooms={property[propType].bathRooms}
+                />
+              </Col>
+            ))
+          )}
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default RentPropertySection;
