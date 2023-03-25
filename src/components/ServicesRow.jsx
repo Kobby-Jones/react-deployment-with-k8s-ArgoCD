44c163cc71d984cfdc077e43a291c1b48476b504
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 03:45:44 +0000

    Rent and Sell Column

--- src/components/ServicesRow.jsx
+++ src/components/ServicesRow.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
-// import ServicesCard from './ServicesCard';
-// import services from './ServicesTexts';
+import { Row, Col } from 'react-bootstrap';
+import ServicesCard from './ServicesCard';
+import services from './ServicesTexts';
  
 function ServicesRow() {
     return (
       <Row lg="4">
+        <Col lg="4">
+          <ServicesCard
+            title={services.rentAndSell.title}
+            text={services.rentAndSell.text}
+          />
+        </Col>
       </Row>
     );
 }
