ebf97d6f47d15887c1ddd1e1ee3ea67cb5361f91
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 01:23:16 +0000

    About Us section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
 import HomePage from './components/Home';
 import CustomNavbar from './components/Navbar';
 import RentPropertySection from './components/RentPropertiesSection';
 import SalesPropertySection from "./components/SalesPropertySection";
 import Testimonials from './components/Testiminials';
 
-
+// sk - w3mzBwsYTda1E0kTVNWET3BlbkFJltpEjVNqaE1vTBKvlXPH;
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Testimonials />
       <SalesPropertySection />
       <RentPropertySection />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function About() {
+    return ( 
+        <section>
+            <div className="services-head text-center">
+                <h2 className='text-uppercase'>Our Services</h2>
+            </div>
+            <Container>
+
+            </Container>
+        </section>
+     );
+}
+
+export default About;
