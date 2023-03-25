dca782fcbdb4e85771de2f34860846b1a433e15e
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 02:13:18 +0000

    Services line separator

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 function Services() {
     return ( 
         <div>
-            <div className="services-head text-center">
+            <div className="services-head text-center py-3">
                 <h2 className="text-uppercae">Our services</h2>
+            </div>
+            <div className="line">
+                <hr
+                    style={
+                        {
+                            width:'100%'
+                        }
+                }
+                />
             </div>
         </div>
      );
