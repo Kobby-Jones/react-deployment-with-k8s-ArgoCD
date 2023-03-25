f861ab84dbe3113c03a83f0bff44bc7246dde5c7
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 02:53:37 +0000

    Import House Icon

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/fontawesome-free'
+
 
 
 function ServicesCard(props) {
     return (
       <div>
         <Card>
-            <Card.Title>
-                <div className="text-center">
-                <h3>{props.title}</h3>
-                </div>
-            </Card.Title>
-            <Card.Body>
-                <div className="text-center">
-                        <p>{ props.text}</p>
-                </div>
-            </Card.Body>
+          <Card.Title>
+            <div className="icon text-center">
+                <FontAwesomeIcon icon="fa-solid fa-house" />
+            </div>
+            <div className="text-center">
+              <h3>{props.title}</h3>
+            </div>
+          </Card.Title>
+          <Card.Body>
+            <div className="text-center">
+              <p>{props.text}</p>
+            </div>
+          </Card.Body>
         </Card>
       </div>
     );
