08131cf39fdee61edc47e76d4fa81fcbd9186454
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 01:33:10 +0000

    Horizontal rule

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 
 function About() {
-    return ( 
-        <section>
-            <div className="services-head text-center">
-                <h2 className='text-uppercase'>Our Services</h2>
-            </div>
-            <Container>
-
-            </Container>
-        </section>
-     );
+    return (
+      <section>
+        <div className="services-head text-center">
+          <h2 className="text-uppercase">Our Services</h2>
+        </div>
+        <div className="line">
+          <hr
+            style={{
+              width: "100%"
+            }}
+          />
+        </div>
+        <Container></Container>
+      </section>
+    );
 }
 
 export default About;
