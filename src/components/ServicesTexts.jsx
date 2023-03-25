c2554fe805a768d73dd4f36af1520a6808238ea8
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 02:36:38 +0000

    Titles included in properties text

---
+++ src/components/ServicesTexts.jsx
@@ -0,0 +1,15 @@
+const services = {
+  rentAndSell: {
+    title: "Renting and Selling Services",
+    text: "Our Renting and Selling Services cater to the needs of both buyers and sellers. We offer a range of marketing and advertising services to help you sell or rent your property quickly and at the best possible price. Our team conducts thorough property evaluations, helps you with property staging, and provides expert advice on pricing strategy. We also provide comprehensive buyer representation services to help you find the perfect property at the best possible price.",
+  },
+  propertyManagement: {
+    title: "Property Management Services",
+    text: "Our Property Management Services are designed to make property ownership a hassle-free experience. We offer a complete suite of management services, including tenant screening and selection, rent collection, property maintenance, and accounting services. We work with a network of reliable contractors and vendors to ensure that your property is always in top condition and that any maintenance issues are addressed promptly.",
+  },
+    propertyListing: {
+      title: "Client Property Listing Services",
+    text: "Client Property Listing Services allow property owners to list their properties with us for sale or rent. We offer a variety of listing options, from basic online listings to full-service marketing campaigns. Our agents will work closely with you to create a customized marketing plan that meets your specific needs and goals.",
+  },
+};
+export default services
