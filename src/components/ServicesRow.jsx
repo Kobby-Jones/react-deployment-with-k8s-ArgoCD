d62ec1a98e81dd73c5dcab5bfb46e077eeebcce9
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 03:39:45 +0000

    Rows for services cards

---
+++ src/components/ServicesRow.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+// import ServicesCard from './ServicesCard';
+// import services from './ServicesTexts';
+ 
+function ServicesRow() {
+    return (
+      <Row lg="4">
+      </Row>
+    );
+}
+
+export default ServicesRow;
