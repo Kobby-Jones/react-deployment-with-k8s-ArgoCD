ddb9a8cfb3c22583a96f6ca0e84ac16889281082
Kobby Jones <kobbyj099@gmail.com>
2023-03-25 02:20:21 +0000

    Who we are text

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -12,23 +12,20 @@ function WhoWeAre() {
             }}
           />
         </div>
-            <div className="text-center">
+        <div className="text-center">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
-            dolorum. Vel, pariatur veniam. Eveniet voluptatum vitae minus
-            explicabo! Mollitia veritatis laudantium eos porro possimus
-            distinctio minus necessitatibus eum ipsa veniam! Iusto, deleniti
-            accusantium quibusdam magnam sunt nisi labore assumenda, vel modi
-            alias nulla quod eligendi facilis, et a exercitationem obcaecati
-            officiis. Voluptatum sequi nostrum illum velit assumenda aperiam
-            unde cupiditate! Natus repellat blanditiis iure similique qui
-            excepturi ex ipsa porro consectetur? Illo eos quisquam sapiente quod
-            ratione officiis quam exercitationem! Aperiam temporibus impedit
-            minima eaque nam earum tenetur, corporis sunt? Natus rerum
-            distinctio perferendis fugiat laborum delectus, accusamus saepe
-            maxime voluptates veniam placeat expedita debitis eligendi, totam
-            magnam accusantium enim soluta ipsum optio vero mollitia quasi
-            consequuntur, explicabo libero? Velit!
+            At KJ Real Estate Agency Ltd, we're passionate about creating
+            exceptional living spaces that meet the needs and desires of our
+            clients. With years of experience in the industry, we're committed
+            to delivering high-quality, sustainable developments that not only
+            enhance the value of the community, but also improve the quality of
+            life for its residents. Our team of experts works closely with
+            architects, designers, and builders to ensure that each project is
+            executed to perfection, from the initial concept to the final
+            touches. At KJ, we take pride in our attention to detail,
+            dedication to excellence, and unwavering commitment to client
+            satisfaction. Contact us today to learn more about our services and
+            how we can help you realize your vision for your dream home.
           </p>
         </div>
       </div>
