7e910ee9aafbb07731c051489c5d2d9e77f473b4
Kobby Jones <kobbyj099@gmail.com>
2023-04-09 03:55:59 +0000

    Gave the customer testimonials some margin

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 const TestimonialCard = (params) => {
-    return (
-      <Card className='text-center p-3'>
-          <Card.Text>
-            {params.text}
-          </Card.Text>
-          <Card.Title>{params.name}</Card.Title>
-      </Card>
-    );
-}
+  return (
+    <Card className='text-center p-3 mb-3'>
+      <Card.Text>
+        {params.text}
+      </Card.Text>
+      <Card.Title>{params.name}</Card.Title>
+    </Card>
+  );
+    }
+
 
 export default TestimonialCard;
