80e75f4c3ab398929bcaa96a93622915eadf20f1
Kobby Jones <kobbyj099@gmail.com>
2023-04-09 02:52:46 +0000

    Updated settings.json file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter } from 'react-router-dom';
@@ -10,7 +9,6 @@ import Routers from './components/Routers';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <BrowserRouter>
     <Routers />
     </BrowserRouter>
