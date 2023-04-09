8284260ee8b6bb1739a8580cd5d867f40cfb52a4
Kobby Jones <kobbyj099@gmail.com>
2023-04-09 02:47:22 +0000

    Render footer with the Services page

---
+++ src/components/ServicesPageWithFooter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import Services from "./Services";
+
+function ServicesPageWithFooter() {
+  return (
+    <div>
+      <Services />
+      <Footer />
+    </div>
+  );
+}
+export default ServicesPageWithFooter;
