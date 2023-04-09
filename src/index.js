176c96b0ac2521c66152cd7e87e55f81173bdbe8
Kobby Jones <kobbyj099@gmail.com>
2023-04-09 02:41:22 +0000

    Updated index.jsx file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
-
+import { BrowserRouter } from 'react-router-dom';
+import Routers from './components/Routers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+    <Routers />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
