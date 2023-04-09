c3a8d830a8912786454159ff736b35867952f568
Kobby Jones <kobbyj099@gmail.com>
2023-04-09 02:48:22 +0000

    Routers

---
+++ src/components/Routers.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom'; 
+import CustomNavbar from './Navbar'
+import ServicesPageWithFooter from './ServicesPageWithFooter';
+import ContactPageWithFooter from './ContactPageWithFooter';
+import App from '../App';
+import RentPropertySection from './RentPropertiesSection';
+import SalesPropertySection from './SalesPropertySection';
+
+function Routers() {
+    return (
+        <div>
+            <CustomNavbar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="about" element={<ServicesPageWithFooter />} />
+          <Route path="contact" element={<ContactPageWithFooter />} />
+          <Route path="rent" element={<RentPropertySection />} />
+          <Route path="sales" element={<SalesPropertySection />} />
+        </Routes>
+      </div>
+    );
+}
+export default Routers;
