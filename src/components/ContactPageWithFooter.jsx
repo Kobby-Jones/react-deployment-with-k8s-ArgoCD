7893cf928c3e37422e51a17ab77b84f243836f14
Kobby Jones <kobbyj099@gmail.com>
2023-04-09 02:45:10 +0000

    Render footer with the Contact page

---
+++ src/components/ContactPageWithFooter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import ContactUs from "./Contact";
+
+function ContactPageWithFooter() {
+  return (
+    <div>
+      <ContactUs />
+      <Footer />
+    </div>
+  );
+}
+export default ContactPageWithFooter;
