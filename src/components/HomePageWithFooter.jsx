da6ba145130ca85d97efa1f5aece0e08ece955fb
Kobby Jones <kobbyj099@gmail.com>
2023-04-09 02:46:21 +0000

    Render footer with the Home page

---
+++ src/components/HomePageWithFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomePage from './Home';
+import Footer from './Footer'
+
+function HomePageWithFooter() {
+    return (
+      <div>
+        <HomePage />
+        <Footer />
+      </div>
+    );
+}
+export default HomePageWithFooter;
