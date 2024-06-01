f04cda5ad646ef46c92052d5ed04de25590e6c4e
Kobby Jones <kobbyj099@gmail.com>
2024-06-01 20:29:07 +0000

    Updated

--- src/components/testimonials/TestimonialArrays.jsx
+++ src/components/testimonials/TestimonialArrays.jsx
@@ -4,21 +4,21 @@ const customers = [
     name: "Kobby Jones",
     image: "https://randomuser.me/api/portraits/men/99.jpg",
     testimony:
-      `"I had a great experience working with the KJ Real Estate Agency team. They were very knowledgeable and helpful throughout the entire process. I would definitely recommend them to anyone looking to buy or sell a property"`,
+      `"I had a great experience working with the Dreamscape Real Estate team. They were very knowledgeable and helpful throughout the entire process. I would definitely recommend them to anyone looking to buy or sell a property"`,
   },
    {
     id: 2,
     name: "Sylvia Roselyne",
     image: "https://randomuser.me/api/portraits/women/89.jpg",
     testimony:
-      `"The KJ Real Estate Agency team made selling my home a breeze. They were professional, responsive, and always had my best interests in mind. I would highly recommend their services to anyone in the market for a new home or looking to sell their existing property."`,
+      `"The Dreamscape Real Estate team made selling my home a breeze. They were professional, responsive, and always had my best interests in mind. I would highly recommend their services to anyone in the market for a new home or looking to sell their existing property."`,
   },
   {
     id: 3,
     name: "Mike Johnson",
     image: "https://randomuser.me/api/portraits/men/89.jpg",
     testimony:
-      `"I cannot speak highly enough of the KJ Real Estate Agency team. They were incredibly helpful and responsive, and went above and beyond to help us find the perfect home for our family. I would definitely work with them again in the future."`,
+      `"I cannot speak highly enough of the Dreamscape Real Estate team. They were incredibly helpful and responsive, and went above and beyond to help us find the perfect home for our family. I would definitely work with them again in the future."`,
   },
 ];
 export default customers;
